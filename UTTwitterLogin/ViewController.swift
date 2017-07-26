55689eee0595365c116f09ead0b35f4e5c8b83d3
Usman Tarar <usman.tarar@nxb.com.pk>
2017-07-26 16:57:10 +0500

    Initial Commit

---
+++ UTTwitterLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UTTwitterLogin
+//
+//  Created by Usman Tarar on 26/07/2017.
+//  Copyright © 2017 Usman Tarar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
